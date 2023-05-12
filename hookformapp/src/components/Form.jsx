2c59d0a415e1b11f2aeafc1be6cfc8485c6f6118
Ricardo Palafox <rpalafox8804@gmail.com>
2023-05-12 14:43:45 -0700

    Adding Validation

--- hookformapp/src/components/Form.jsx
+++ hookformapp/src/components/Form.jsx
@@ -19,22 +19,27 @@ const Form = () => {
         {/* form submission */}
         <form action="" className='col-4 mx-auto'>
             <div className='form-group'>
+                {user.firstName && user.firstName.length < 2 ? <p className='text-danger'>First Name must be at least 2 characters</p> : null}
                 <label htmlFor="firstName" className='form-label'>First Name:</label>
                 <input type="text" name="firstName" id='firstName' className='form-control' onChange={changeHandler}/>
             </div>
             <div className='form-group'>
+                {user.lastName && user.lastName.length < 2 ? <p className='text-danger'>Last Name must be at least 2 characters</p> : null }
                 <label htmlFor="lastName" className='form-label'>Last Name:</label>
                 <input type="text" name="lastName" id='lastName' className='form-control' onChange={changeHandler}/>
             </div>
             <div className='form-group'>
+                {user.email && user.email.length < 2 ? <p className='text-danger'>Email must be at least 2 characters</p> : null}
                 <label htmlFor="email" className='form-label'>Email:</label>
                 <input type="email" name="email" id='email' className='form-control' onChange={changeHandler}/>
             </div>
             <div className='form-group'>
+                {user.password && user.password.length < 8 ? <p className='text-danger'>Password must be at least 8 characters</p> : null }
                 <label htmlFor="password" className='form-label'>Password:</label>
                 <input type="password" name="password" id='password' className='form-control'onChange={changeHandler}/>
             </div>
             <div className='form-group'>
+                {user.confirmPassword && user.confirmPassword !== user.password ? <p className='text-danger'>Passwords must match</p> : null}
                 <label htmlFor="confirmPassword" className='form-label'>Confirm Password:</label>
                 <input type="password" name="confirmPassword" id='confirmPassword' className='form-control'onChange={changeHandler}/>
             </div>
